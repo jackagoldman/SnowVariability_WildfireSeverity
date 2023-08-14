53ef7385a35e83f17dcaa23c736a0a5e06fa621c
jackagoldman <jandrewgoldman@gmail.com>
2023-08-14 10:09:58 -0400

    update

--- scripts/sem-table-function.R
+++ scripts/sem-table-function.R
@@ -3,6 +3,35 @@
 require(tidyverse)
 require(kableExtra)
 
+sem_results <- function(x){
+  x2<- summary(x)$coefficients
+  x2$Estimate <- round(x2$Estimate, 3)
+  x2$Std.Error <- round(x2$Std.Error, 3)
+  x2$Crit.Value <- round(x2$Crit.Value, 3)
+  x2$P.Value <- round(x2$P.Value, 3)
+  x2$P.Value <- ifelse(x2$P.Value<0.001, "<0.001", x2$P.Value)
+  x2$Std.Estimate <- round(x2$Std.Estimate, 3)
+  x2$Response <- gsub("_", " ", x2$Response)
+  x2$Predictor <- gsub("_", " ",x2$Predictor)
+  colnames(x2)[4:8] <- c("SE", "DF", "t-Value", "P-Value", "Std. Estimate")
+  x2<- data.frame(x2)
+  x3<- x2 %>% mutate(Response = case_when(
+    Response == "RBR median" ~"Severity",
+    Response == "rbr qs" ~ "Severity",
+    Response == "sdd" ~ "SFD",
+    Response == "tssm" ~ "SFDr"),
+    Predictor = case_when(
+      Predictor == "tri" ~ "Topo",
+      Predictor == "dc" ~ "Drought",
+      Predictor == "cc" ~ "Canopy Closure",
+      Predictor == "age" ~ "Stand Age",
+      Predictor == "avgBio" ~ "Biomass", 
+      Predictor == "sdd" ~ "SFD",
+      Predictor == "tssm" ~ "SFDR",
+    ))
+  colnames(x3)[9] <-""
+  return(x3)
+}
 
 
 sem_table <- function(x, region)
@@ -73,4 +102,39 @@ x_table <- x3 %>%
 
 
 return(x_table)
+}
+
+# cv
+sem_table_cv <- function(x){
+  x2<- summary(x)$coefficients
+  x2$Estimate <- round(x2$Estimate, 3)
+  x2$Std.Error <- round(x2$Std.Error, 3)
+  x2$Crit.Value <- round(x2$Crit.Value, 3)
+  x2$P.Value <- round(x2$P.Value, 3)
+  x2$P.Value <- ifelse(x2$P.Value<0.001, "<0.001", x2$P.Value)
+  x2$Std.Estimate <- round(x2$Std.Estimate, 3)
+  x2$Response <- gsub("_", " ", x2$Response)
+  x2$Predictor <- gsub("_", " ",x2$Predictor)
+  colnames(x2)[4:8] <- c("SE", "DF", "t-Value", "P-Value", "Std. Estimate")
+  x2<- data.frame(x2)
+  x3<- x2 %>% mutate(Response = case_when(
+    Response == "RBR median" ~"Severity",
+    Response == "rbr qs" ~ "Severity",
+    Response == "sdd" ~ "SFD",
+    Response == "tssm" ~ "SFDr"),
+    Predictor = case_when(
+      Predictor == "tri" ~ "Topo",
+      Predictor == "dc" ~ "Drought",
+      Predictor == "cc" ~ "Canopy Closure",
+      Predictor == "age" ~ "Stand Age",
+      Predictor == "avgBio" ~ "Biomass", 
+      Predictor == "sdd" ~ "SFD",
+      Predictor == "tssm" ~ "SFDR",
+    ))
+  colnames(x3)[9] <-""
+  x_table <- x3 %>% 
+    kbl(caption = "piecewiseSEM results for heterogeneity in burn severity for fires greater than 500 ha", 
+        booktabs = T) %>% 
+    kable_classic(full_width = F, html_font = "Cambria") 
+  return(x_table)
 }
